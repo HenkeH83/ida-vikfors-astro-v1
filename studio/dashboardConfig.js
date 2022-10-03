a5ff854aa50891b5575505d29d8f81e9630e3cff
Henrik <henrik.hagstrom@layer10.se>
2022-10-03 21:53:22 +0200

    Update dashboardConfig

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -5,12 +5,12 @@ export default {
             options: {
                 title: 'Netlify deploys',
                 sites: [
-                    // {
-                    //     title: 'Sanity Studio',
-                    //     apiId: '90b24aec-810e-4953-a61c-a049c855d011',
-                    //     buildHookId: '602e2e73b2a306b29f38a0ac',
-                    //     name: 'vigorous-montalcini-4acbe7.netlify.app',
-                    // },
+                    {
+                        title: 'Sanity Studio',
+                        apiId: '90b24aec-810e-4953-a61c-a049c855d011',
+                        buildHookId: '602e2e73b2a306b29f38a0ac',
+                        name: 'vigorous-montalcini-4acbe7.netlify.app',
+                    },
                     {
                         title: 'Website',
                         apiId: 'e2d0123a-f497-4680-af5e-14904b6e59ba',
