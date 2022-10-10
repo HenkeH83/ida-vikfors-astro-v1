129199ac7378528a38bf8a443cb7404bdf113a9d
Henrik <henrik.hagstrom@layer10.se>
2022-10-10 16:48:17 +0200

    typescript onclick_outside fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+
+// fix från: https://fix.code-error.com/svelte-custom-event-on-svelte-typescript/
+declare namespace svelte.JSX {
+    interface HTMLAttributes<T> {
+      onclick_outside: () => void
+    }
+  }
